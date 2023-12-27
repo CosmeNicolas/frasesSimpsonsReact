08ff3356fb1cb9f923b835d0f1cb125baff2a304
CosmeNicolas <nicomas7590@gmail.com>
2023-12-26 21:54:04 -0300

    agregue el componente card de bootsrap react para darle contraste el texto que esta debajo de la imagen

--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import homero from "../img/homero.png"
+import { Card } from 'react-bootstrap';
 
 const Frase = () => {
   return (
     <>
       <h1 className='text-center text-light'>Homero</h1>
       <img className='img-fluid' src={homero} alt=""/>
+      <Card>
+         <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card>
     </>
   );
 };
