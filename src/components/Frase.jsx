e6467f88f91c9774b688470809a1459c77c94c6d
CosmeNicolas <nicomas7590@gmail.com>
2023-12-27 20:19:46 -0300

    cambie  la descripcion de la card

--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -11,8 +11,7 @@ const Frase = () => {
       <img className='img-fluid' src={homero} alt=""/>
       <Card>
          <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          Aqui se colocara la frase simpsons de cada personaje 
         </Card.Text>
       </Card>
     </div>
