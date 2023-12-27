07a62c292a0ba43f0aaaf268aee233bd6a092e75
CosmeNicolas <nicomas7590@gmail.com>
2023-12-26 22:15:54 -0300

    agregue un hr entre el titulo y la imagen

--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -5,7 +5,9 @@ import { Card } from 'react-bootstrap';
 const Frase = () => {
   return (
     <>
+    <div className='border border-danger'>
       <h1 className='text-center text-light'>Homero</h1>
+      <hr  className='mx-5'/>
       <img className='img-fluid' src={homero} alt=""/>
       <Card>
          <Card.Text>
@@ -13,6 +15,7 @@ const Frase = () => {
           bulk of the card's content.
         </Card.Text>
       </Card>
+    </div>
     </>
   );
 };
